d15087147ebf662972a93fc1be2cfc66a5b3ff4f
suxss <82267359+suxss@users.noreply.github.com>
2021-10-22 18:10:22 +0800

    Update serverless.js

--- src/serverless.js
+++ src/serverless.js
@@ -32,6 +32,6 @@ exports.handler = apolloServer.createHandler({
 		credentials: true,
 		methods: 'GET,POST,PATCH,OPTIONS',
 		allowedHeaders: 'Content-Type, Authorization, Time-Zone',
-		Access-Control-Allow-Origin: "https://blog.suxss.tk"
+		AccessControlAllowOrigin: "https://blog.suxss.tk"
 	},
 })
